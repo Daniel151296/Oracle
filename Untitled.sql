8277301d40ff812e8a0007a69befdb66798974b2
labsfiap <labsfiap@PA502MICRO19.fiap.com.br>
2024-08-15 09:45:07 -0300

    Aula 15-08-2024

--- Untitled.sql
+++ Untitled.sql
@@ -56,3 +56,52 @@ PAIS_ID NUMBER PRIMARY KEY,
 NOME_PAIS VARCHAR2(100) NOT NULL      
 );
 
+
+
+
+
+-- visualizar tabela de outro usuario
+Select *
+from pf1788.endereco_cliente;
+
+
+Select distinct cod_tipo_endereco
+from pf1788.endereco_cliente;
+
+SELECT cod_tipo_endereco, count(1)
+from pf1788.endereco_cliente
+group by cod_tipo_endereco
+
+Select count(1),des_bairro
+from pf1788.endereco_cliente
+group by des_bairro
+
+Select count(1),des_bairro
+from pf1788.endereco_cliente
+group by des_bairro
+having count(1) > 5
+
+Select count(1),des_bairro
+from pf1788.endereco_cliente
+group by des_bairro
+having count(1) between 1 and 5;
+
+------------------------------------------------------------------
+
+
+set SERVEROUTPUT on;
+
+DECLARE
+    salario NUMBER := 1412;
+    --v_nome VARCHAR2(30);
+    --v_idade number := 39;
+    --v_number1 NUMBER := 10;
+    --v_number2 NUMBER := 8;
+
+BEGIN
+    dbms_output.put_line('O valor do novo salario minimo é: '|| (salario*1.25));
+    --v_nome := 'Daniel';
+    --dbms_output.put_line(v_number1 + v_number2);
+    --dbms_output.put_line('Nome ' || v_nome || ' Idade ' || v_idade);
+
+END;
